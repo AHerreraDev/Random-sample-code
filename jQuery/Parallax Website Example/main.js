7fd686177e2f9391334dd7bd260c3c3051b84417
Alex Herrera <alejandrossh@hotmail.com>
2017-09-27 10:02:30 -0500

    a Simple parallax website example

---
+++ jQuery/Parallax Website Example/main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+		  Particles.init({
+		    selector: '.background',
+		    maxParticles: 180,
+		    breakpoint: 150,
+		    color: '#3066BE',
+		    connectParticles: true,
+		  });
+		};
+
